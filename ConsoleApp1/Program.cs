60db96750c181ac475bde591bba0d0261608e0ef
visualstudio <visualstudio@230-PC28>
2019-01-17 15:37:50 -0500

    make it beautiful

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿sing System;
 
-namespace ConsoleApp1
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var red = new Blue("Roy", 17);
+        // how to construct OBJECTS??? 
+        // a constructor is a special method that constructs objects!!!! 
+        Console.WriteLine(red.addNumbers());
+        Console.ReadLine();
+    }
+}
+
+class Blue
+{
+    public Blue()
+    {
+        Console.WriteLine("I am in the NO ARGUMENTS. ");
+    }
+
+    public Blue(string a, int b)
+    {
+        Console.WriteLine("{O} {1} ", a, b);
+    }
+    public int addNumbers()
+    {
+        int i;
+        int sum = 0;
+        for (i = 618; i >= 211; i -= 18)
         {
-            Console.WriteLine("Hello World!");
+            sum += i;
         }
+        return sum;
     }
 }
